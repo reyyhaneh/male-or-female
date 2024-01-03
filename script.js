1e49daa11e1860f92c43f16eb55f615a34fea0e3
reyhoondavaee@gmail.com <reyhoondavaee@gmail.com>
2024-01-03 23:08:55 +0330

    gender predictions are shown in the prediction column

--- script.js
+++ script.js
@@ -11,5 +11,29 @@ function submitForm() {
     }
 }
 
+
 function fetchDataFromApi(name) {
+    const apiEndpoint = 'https://api.genderize.io/'; 
+
+    fetch(`${apiEndpoint}?name=${encodeURIComponent(name)}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            updatePredictionColumn(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data from API:', error);
+        });
+}
+
+function updatePredictionColumn(prediction) {
+    const predictionColumn = document.getElementById('gender');
+    const gender = prediction.gender;
+    const probability = prediction.probability;
+
+    predictionColumn.innerHTML = `<p>${gender}</p> <p>${probability}</p>`;
 }
